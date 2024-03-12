83a784562a528d61a074b97eac6557b5497df8bd
MohamedShehata86700 <129014410+MohamedShehata86700@users.noreply.github.com>
2024-03-12 16:12:56 +0200

    Update main.py

--- main.py
+++ main.py
@@ -88,7 +88,7 @@ def cart():
 		   		ncharge=(ch[0]['value'])
 		   		vcharge=(ch[1]['value'])
 		   		cnum=(ch[3]['value'])
-		   		if float(vcharge) > 20:
+		   		if float(vcharge) > 200:
 		   			if vcharge not in printed_values:
 		   				printed_values.append(vcharge)	
 		   		
